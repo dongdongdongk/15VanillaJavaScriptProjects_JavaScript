13de3353f3b70d11ba070f50da368056c46013e3
dongdongdongk <dhk9309@gmail.com>
2023-10-02 18:18:55 +0900

    pj02

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -8,8 +8,8 @@ const btns = document.querySelectorAll('.btn');
 //forEach 로 위의 btns 에 저장된 class btn 들의 배열 
 btns.forEach(function (btn){ // btns 의 각 값을 btn 으로 지정하고 사용 
     btn.addEventListener('click', function(e){ // 각 btn 을 클릭 할 때 클릭한 대상을 e로 지정하고 사용 
-        const styles = e.currentTarget.classList
-        if(styles.contains('decrease')){
+        const styles = e.currentTarget.classList // 클릭한 타겟이 가지고 있는 클래스들 전부 배열로(classList)
+        if(styles.contains('decrease')){ // classList 로 받은 배열에 ex decrease 가 포함되어 있다면 
             count--;         
         }else if(styles.contains('increase')) {
             count++;
